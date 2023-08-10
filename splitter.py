eab2268b0696be3cbe1f42a23c694b8f7d5375b8
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2023-08-11 01:20:28 +0300

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -3,48 +3,49 @@ import re
 
 
 
-input_video_path = input('Path to the video: ')
-timestamps_file = input('Path to the timestamps file: ')
-names = []
-timestamps = []
+input_video_path = '/home/elneklawy/Desktop/a.mp4'
+timestamps_file = '/home/elneklawy/Desktop/ts'
 timestamps_scnds = []
-pattern = r'(\d+:\d+:\d+)[^\w]*'
 video_clip = VideoFileClip(input_video_path)
+
+str_pattern = r'^\d:\d\d\s*[-_!-*\s]*\s*(.*?)\s*(?=\d:\d\d\s*[-_!-*\s]*|$)'
+ts_pattern = r'^(\d:\d\d)\s*[-_!-*\s]*\s*.*$'
 
 
 with open(timestamps_file, 'r') as f:
-    for line in f:
-        l = line.split(maxsplit=1)
-        names.append(l[-1].removesuffix('\n'))
-        timestamps.append(l[0])
+    lines = f.readlines()
 
-max_len = len(timestamps[-1].split(':'))
+lines = ''.join(lines)
+
+chapter_names = re.findall(str_pattern, lines, re.MULTILINE)
+ts = re.findall(ts_pattern, lines, re.MULTILINE)
+
+max_len = len(ts[-1].split(':'))
 if max_len == 2:
-    for i in range(len(timestamps)):
-        timestamps[i] = timestamps[i].strip()
-        #timestamps[i] = '00:' * (max_len - len(timestamps[i].split(':'))) + timestamps[i]
-        timestamps[i] = '00:' + timestamps[i]
+    for i in range(len(ts)):
+        ts[i] = ts[i].strip()
+        #ts[i] = '00:' * (max_len - len(ts[i].split(':'))) + ts[i]
+        ts[i] = '00:' + ts[i]
 else:
-    for i in range(len(timestamps)):
-        timestamps[i] = timestamps[i].strip()
+    for i in range(len(ts)):
+        ts[i] = ts[i].strip()
 
-for i in range(len(timestamps)):
-    res = re.search(pattern, timestamps[i])
-    ts = res.group(1)
-    splitted = re.split(':', ts)
+for i in range(len(ts)):
+    splitted = ts[i].split(':')
     hrs, mins, scnds = int(splitted[0]), int(splitted[1]), int(splitted[2])
     total_scnds = hrs * 60 * 60 + mins * 60 + scnds
     timestamps_scnds.append(total_scnds)
 
+
 for i, start in enumerate(timestamps_scnds):
     if i < len(timestamps_scnds) - 1:
         end = timestamps_scnds[i + 1]
-        output_path = f"{i+1}- {names[i]}.mp4"
+        output_path = f"{i+1}- {chapter_names[i]}.mp4"
         sub_clib = video_clip.subclip(start, end)
         sub_clib.write_videofile(output_path, audio_codec='aac', fps=60)
 
 end = timestamps_scnds[-1]
-output_path = f"{len(timestamps_scnds)}- {names[-1]}.mp4"
+output_path = f"{len(timestamps_scnds)}- {chapter_names[-1]}.mp4"
 sub_clib = video_clip.subclip(timestamps_scnds[-1])
 sub_clib.write_videofile(output_path, audio_codec='aac', fps=60)
 
