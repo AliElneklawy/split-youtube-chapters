503d641d02199d6f34a538a0fdaddc4b24164000
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2025-07-15 11:39:03 +0300

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -18,6 +18,9 @@ timestamps, chapter_names = zip(*matches)
 ts = list(timestamps)
 chapter_names = list(chapter_names)
 
+# print(ts, chapter_names)
+# exit()
+
 for i in range(len(chapter_names)):
     chapter_names[i] = re.sub(r'[^\w\s-]', ' ', chapter_names[i])
     chapter_names[i] = re.sub(r'\s+', ' ', chapter_names[i])
@@ -29,10 +32,12 @@ for i in range(len(ts)):
         ts[i] = '00:' + ts[i]
     
     splitted = ts[i].split(':')
+    # print(splitted)
     hrs, mins, scnds = int(splitted[0]), int(splitted[1]), int(splitted[2])
     total_scnds = hrs * 60 * 60 + mins * 60 + scnds
     timestamps_scnds.append(total_scnds)
 
+# exit()
 for i, start in enumerate(timestamps_scnds):
     if i < len(timestamps_scnds) - 1:
         end = timestamps_scnds[i + 1] 
@@ -40,14 +45,14 @@ for i, start in enumerate(timestamps_scnds):
         output_path = os.path.join(os.path.dirname(input_video_path), fname)
 
         if not os.path.exists(output_path):
-            sub_clib = video_clip.subclip(start, end)
+            sub_clib = video_clip.subclipped(start, end)
             sub_clib.write_videofile(output_path, audio_codec='aac', fps=60)
 
 end = timestamps_scnds[-1]
 fname = f"{len(timestamps_scnds)}- {chapter_names[-1]}.mp4"
 output_path = os.path.join(os.path.dirname(input_video_path), fname)
 if not os.path.exists(output_path):
-    sub_clib = video_clip.subclip(timestamps_scnds[-1])
+    sub_clib = video_clip.subclipped(timestamps_scnds[-1])
     sub_clib.write_videofile(output_path, audio_codec='aac', fps=60)
 
 video_clip.reader.close()
